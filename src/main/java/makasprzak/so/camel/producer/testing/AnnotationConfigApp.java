a3a9f4b396f107efec951af7a36bf7436a0e2d76
makasprzak <mail.maciej.kasprzak@gmail.com>
2015-05-15 12:05:31 +0200

    Applying answer by Itsallas

--- src/main/java/makasprzak/so/camel/producer/testing/AnnotationConfigApp.java
+++ src/main/java/makasprzak/so/camel/producer/testing/AnnotationConfigApp.java
@@ -1,33 +1,22 @@
 package makasprzak.so.camel.producer.testing;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.spring.SpringCamelContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AnnotationConfigApp {
+public class AnnotationConfigApp extends CamelConfiguration {
    public static void main(String[] args) {
       ApplicationContext context = new AnnotationConfigApplicationContext(AnnotationConfigApp.class);
       SimpleProducer simpleProducer = context.getBean(SimpleProducer.class);
       simpleProducer.send("Hello World!");
    }
 
-   @Autowired
-   private ApplicationContext ctx;
-
    @Bean
    public SimpleProducer simpleProducer() {
       return new SimpleProducer();
    }
 
-   @Bean
-   public CamelContext camelContext() throws Exception {
-      CamelContext camelContext = new SpringCamelContext(ctx);
-      camelContext.start();
-      return camelContext;
-   }
 }
